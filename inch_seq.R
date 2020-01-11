87c3f66aff9002222f30f0e9cbb38b1145aa75be
Mike Vlah <vlahm13@gmail.com>
2020-01-11 17:56:24 -0500

    added inch_seq

---
+++ inch_seq.R
@@ -0,0 +1,21 @@
+inch_seq = function(tot_len, piece_len){
+
+    require(stringr)
+    require(schoolmath)
+
+    comp = str_match(piece_len, '([0-9]+) ([0-9]+)/([0-9]+)')[,2:4]
+    comp = as.numeric(comp)
+
+    if(any(is.na(comp))){
+        stop('piece_len must be of the form "X X/X" where all X are numeric')
+    }
+
+    s = seq(0, total_len, comp[1] + (comp[2]/comp[3]))
+    r = s %% 1 * comp[3]
+    fract_inches = paste0(floor(s), ' ', r, '/', comp[3])
+
+    return(fract_inches[-1])
+}
+
+inch_seq(36, '3 5/8')
+inch_seq(36, '3 5/16')
