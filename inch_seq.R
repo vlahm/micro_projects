5f8aac8a20f2c9693b3c3aa85ce0f077c3204bd5
Mike Vlah <vlahm13@gmail.com>
2020-01-13 10:38:23 -0500

    fixed bugs in inch_seq

--- inch_seq.R
+++ inch_seq.R
@@ -1,7 +1,6 @@
 inch_seq = function(tot_len, piece_len){
 
     require(stringr)
-    require(schoolmath)
 
     comp = str_match(piece_len, '([0-9]+) ([0-9]+)/([0-9]+)')[,2:4]
     comp = as.numeric(comp)
@@ -10,7 +9,7 @@ inch_seq = function(tot_len, piece_len){
         stop('piece_len must be of the form "X X/X" where all X are numeric')
     }
 
-    s = seq(0, total_len, comp[1] + (comp[2]/comp[3]))
+    s = seq(0, tot_len, comp[1] + (comp[2]/comp[3]))
     r = s %% 1 * comp[3]
     fract_inches = paste0(floor(s), ' ', r, '/', comp[3])
 
