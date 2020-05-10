fcfb5a2bab59cf45a6203a1a46dd2d670c00c023
Mike Vlah <vlahm13@gmail.com>
2020-05-10 18:04:27 -0400

    lifelist updater is now rolling zesty

--- lifelist_updater.R
+++ lifelist_updater.R
@@ -1,10 +1,13 @@
-library(tidyverse)
+suppressMessages(library(tidyverse))
 
 #this processing sequence uses lifelist_srs.csv, which was originally
 #generated from "Life List.doc", Manual modifications have been made since then.
 
+#TODO: have it check all common names against the database each time.
+#if NAs result, that indicates that a species has been split/merged
+
 args = commandArgs(trailingOnly=TRUE)
-# args = list(ll_path='~/Desktop/stuff_2/Databases/lifelist_srs.csv')
+# args = c(ll_path='~/Desktop/stuff_2/Databases/lifelist_srs.csv')
 
 #get lifelist
 ll = read.csv(args[1], stringsAsFactors=FALSE)
@@ -16,6 +19,10 @@ req = httr::GET(paste0('http://ebird.org/ws1.1/ref/taxa/ebird?cat=species,',
 txt = httr::content(req, as="text")
 ebrd = jsonlite::fromJSON(txt, simplifyDataFrame=TRUE, flatten=TRUE)
 
+# lookup = 'Red-cockaded Woodpecker'
+# lookup = 'donkey'
+# location='johnnytown'; date='2020-05-10'
+# notes='sweet'
 update_lifelist = function(){
 
     # lookup = readline('Enter common name as e.g. Adj-noun Adj-Noun (no quotes) > ')
@@ -52,27 +59,39 @@ update_lifelist = function(){
 
     ll = bind_rows(newrow, ll) %>%
         arrange(familySciName, sciName) %>%
-        select(comName, sciName, date, location, familySciName, familyComName,
-            speciesCode, notes)
+        select(comName, sciName, familySciName, familyComName, speciesCode,
+            date, location, notes)
 
     write.csv(ll, args[1], row.names=FALSE)
 
-    message('Success')
+    cnt = sum(!is.na(ll$sciName))
+
+    message(paste0('Success! Species count: ', cnt))
 }
 
 update_lifelist()
 
-#correct hyphenated modifiers in common names in lifelist to X-x format
-# ll$common_name = sub('\\-([A-Z])', '-\\L\\1', ll$common_name, perl=TRUE)
-
-# ll = ebrd %>%
+# #correct hyphenated modifiers in common names in lifelist to X-x format
+# ll$comName = sub('\\-([A-Z])', '-\\L\\1', ll$comName, perl=TRUE)
+# ll$comName[ll$comName == 'Slaty-backed Nightingale-thrush'] =
+#     'Slaty-backed Nightingale-Thrush'
+# ll$comName[ll$comName == 'Eastern Screech-owl'] =
+#     'Eastern Screech-Owl'
+# ll$comName[ll$comName == 'California Scrub-jay'] =
+#     'California Scrub-Jay'
+# ll$comName[ll$comName == 'Malaysian Pied-fantail'] =
+#     'Malaysian Pied-Fantail'
+# ll$comName[ll$comName == 'Eastern Wood-pewee'] =
+#     'Eastern Wood-Pewee'
+#
+# ll2 = ebrd %>%
 #     select(sciName, comName, speciesCode, familyComName, familySciName) %>%
-#     right_join(ll, by=c('comName'='common_name')) %>%
+#     right_join(ll, by='comName') %>%
 #     arrange(familySciName, sciName) %>%
 #     select(comName, sciName, date, location, familySciName, familyComName,
 #         speciesCode, notes)
-
-# write.csv('~/Desktop/stuff_2/Databases/lifelist_srs.csv', row.names=FALSE)
 #
-# any(is.na(ll$sciName))
-# ll[which(is.na(qq$sciName)),]
+# ll2[which(is.na(ll2$sciName)),]
+# ebrd$comName[grep("Malaysian", ebrd$comName)]
+#
+# write.csv(ll2, '~/Desktop/stuff_2/Databases/lifelist_srs.csv', row.names=FALSE)
