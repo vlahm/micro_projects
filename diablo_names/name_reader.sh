1cf0754fc4e4ea45977b62a7f95234651c26998d
Mike Vlah <vlahm13@gmail.com>
2024-07-19 09:55:58 -0600

    gifs rendering

--- diablo_names/name_reader.sh
+++ diablo_names/name_reader.sh
@@ -3,6 +3,17 @@
 VOICES=("male2" "male3" "female1" "female3" "child_male" "child_female")
 IMAGE_DIR="$HOME/git/micro_projects/diablo_names/images"
 
+exec 2>/dev/null
+
+# Function to handle cleanup on exit
+cleanup() {
+    pkill -f gifview
+	exit 0
+}
+
+# Set trap to catch SIGINT (Ctrl-C) and call cleanup
+trap cleanup SIGINT
+
 while true; do
 
     RATE=$(( RANDOM % 101 - 70 ))
@@ -12,13 +23,14 @@ while true; do
     VOICE=${VOICES[$RANDOM % ${#VOICES[@]}]}
 
     IMAGE=$(/usr/bin/ls "$IMAGE_DIR"/*.gif | shuf -n 1)
-	feh -Zxg 400x400 -B black $IMAGE >/dev/null 2>&1 &
+	#feh -Zxg 400x400 -B white $IMAGE &
+	gifview -a --title ___ --bg white +e $IMAGE &
 
-    FEH_PID=$!
+    IMG_PID=$!
 
     source ~/git/micro_projects/diablo_names/d2name.sh | spd-say -e -r $RATE -p $PITCH -R $RANGE -i $VOLUME -t $VOICE -w
-    #sleep 0.1
+    sleep 0.2
 
-	kill feh >/dev/null 2>&1
+	kill $IMG_PID
 done
 
